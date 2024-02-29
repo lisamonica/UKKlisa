0dda4968ab295fc66cc776c7d6a17bc24a92b944
HABIBRPL <115853212+lisamonica@users.noreply.github.com>
2024-02-29 14:56:28 +0700

    commit1
    kabataku

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.ukk_lisa"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.ukk_lisa"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
